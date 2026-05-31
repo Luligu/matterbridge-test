03361db19fd2f372a5a6ae7f35304bc0a814980d
Luligu <132135057+Luligu@users.noreply.github.com>
2026-05-31 12:20:29 +0200

    Update dummy.test.ts

--- vitest/dummy.test.ts
+++ vitest/dummy.test.ts
@@ -1,6 +1,5 @@
 import initializePlugin, { TestPlatform, type TestPlatformConfig } from '../src/module.js';
 
-
 describe('TestPlatform', () => {
   it('dummy test', () => {
     expect('matterbridge').toBe('matterbridge');
