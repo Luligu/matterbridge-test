4cef972a9f07d33dbac3a0833ebc21901afbd3fb
Luligu <132135057+Luligu@users.noreply.github.com>
2026-05-31 12:07:02 +0200

    Update module.ts

--- src/module.ts
+++ src/module.ts
@@ -80,8 +80,8 @@ export class TestPlatform extends MatterbridgeDynamicPlatform {
     super(matterbridge, log, config);
 
     // Verify that Matterbridge is the correct version
-    if (typeof this.verifyMatterbridgeVersion !== 'function' || !this.verifyMatterbridgeVersion('3.8.0')) {
-      throw new Error(`The test plugin requires Matterbridge version >= "3.8.0". Please update Matterbridge to the latest version in the frontend.`);
+    if (typeof this.verifyMatterbridgeVersion !== 'function' || !this.verifyMatterbridgeVersion('3.7.0')) {
+      throw new Error(`The test plugin requires Matterbridge version >= "3.7.0". Please update Matterbridge to the latest version in the frontend.`);
     }
 
     this.log.info('Initializing platform:', this.config.name);
